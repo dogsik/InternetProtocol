5295d81aebd241227da8201e96b49c7e601c3fbb
dogsik <83946345+dogsik@users.noreply.github.com>
2022-01-20 00:05:16 +0300

    Добавлены тестовые данные

---
+++ main-project/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Laboratory work #8. GIT\n";
+    cout << "Variant #8. Internet Protocol\n";
+    cout << "Author: Kozlova Anastasia\n";
+    cout << "Group: 14\n";
+    return 0;
+}
